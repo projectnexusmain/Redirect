02e817f9853f0326575d12c93d5ef4c3083d2d80
projectnexusmain <projectnexusfn13@gmail.com>
2025-12-15 19:07:21 +0200

    Update Main.m

--- iOS/Main.m
+++ iOS/Main.m
@@ -3,7 +3,7 @@
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 
-#define API_URL @"https://voltronite-backend.onrender.com/"
+#define API_URL @"http://100.87.53.47"
 #define EPIC_GAMES_URL @"ol.epicgames.com"
 
 @interface CustomURLProtocol : NSURLProtocol
